a13624178d0725e7ddb5da82edb602efaa58cb5a
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-01-30 00:58:53 -0500

    update
    Polished enough to publish. Changed theme to sky blue and changed styles accordingly. Implemented social links in header as well. Responsive design mostly done, could improve styling of created QR codes

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
+//Assign variables to form element and QR code output element
 const formEl = document.getElementById('generate-form')
 const qrEl = document.getElementById('qrcode')
 
+
 const onGenerateSubmit = (e) => {
+    
+    //Need this to prevent the default action of the form
     e.preventDefault();
 
+    //Call this method early in order to clear out previously generated QR code and remove Save QR Code button.
     clearUI();
 
+    //Assigning values from User's input to variables
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
     
-   
-
+    //First condition is form validation to ensure user is entering a URL. Sweet Alert will alert user if no URL is entered.
     if (url === "") {
-        alert('Please enter a URL')
+        Swal.fire({
+            title: 'Woops!',
+            text: 'Please enter a URL',
+            icon: 'error',
+            confirmButtonText: 'Ok',
+            confirmButtonColor: "#24b1f1",
+          });
     } else {
+        //Show spinner for 1.5 seconds before transitioning to the generated QR code.
         showSpinner()
-
         setTimeout(() => {
             hideSpinner(); 
-
              generateQRCode(url, size);
-
+             
+             //Need SetTimeout function for slight delay to add src data from QR code to Save QR Code button. 
              setTimeout(()=> {
                 const saveURL = qrEl.querySelector('img').src;
                 saveQRCodeBtn(saveURL)
@@ -30,6 +41,7 @@ const onGenerateSubmit = (e) => {
     }   
 };
 
+//Utilizing QRCode.js library to generate QR code and assign it's img to the div with id of 'qrcode' based on options passed in. 
 const generateQRCode = (url, size) => {
     const qrcode = new QRCode("qrcode", {
         text: url,
@@ -46,21 +58,22 @@ const hideSpinner = () => {
     document.getElementById('spinner').style.display = 'none';
 }
 
-// Clear out QR Code
+// Clear out QR Code and save button
 const clearUI = () => {
     qrEl.innerHTML= "";
-    const saveBtn = document.getElementById('save-link')
-    if(saveBtn) saveBtn.remove();
+    const saveQRBtn = document.getElementById('save-link')
+    if(saveQRBtn) saveQRBtn.remove();
 }
 
+//Creating custom link element that will allow user to download QR code. href == (src from QR code's img.src ). Passed into function from as parameter since it's defined by user.
 const saveQRCodeBtn = (saveURL) => {
-    const link = document.createElement('a');
-    link.id = 'save-link';
-    link.classList = 'bg-red-500 hover:bg-red-700 text-white font-bold py-2 m-auto my-5 rounded w-1/3';
-    link.href = saveURL;
-    link.download = 'qrcode';
-    link.innerHTML = 'Save QR Code';
-    document.getElementById('generated').appendChild(link)
+    const saveQRBtn = document.createElement('a');
+    saveQRBtn.id = 'save-link';
+    saveQRBtn.classList = 'text-white font-semibold md:text-xl py-2 m-auto my-5 mt-10 w-2/3 rounded bg-gradient-to-b from-sky-500 to-sky-400 hover:bg-sky-600 ';
+    saveQRBtn.href = saveURL;
+    saveQRBtn.download = 'qrcode';
+    saveQRBtn.innerHTML = 'Save QR Code';
+    document.getElementById('generated').appendChild(saveQRBtn)
 };
 
 //Calling hideSpinner function as another layer to ensure Spinner is not showing upon load. Style is also set to display:none in html
